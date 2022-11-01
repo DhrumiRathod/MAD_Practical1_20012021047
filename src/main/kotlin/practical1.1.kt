fb4cd92b6e214159e780bf46bac293316bc4484c
DhrumiRathod <dhrumirathod@gmail.com>
2022-11-01 20:01:20 +0530

    final commit1

---
+++ src/main/kotlin/practical1.1.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    var x : Int=45
+    println("Integer Value : $x ")
+    var y = 45.5
+    println("Float Value : $y ")
+    var z : Char= 'D'
+    println("Character Value : $z ")
+    var s : String="Dhrumi"
+    println("String Value : $s ")
+    var b : Boolean= false
+    println("Boolean value : $b ")
+    var d : Double=96.36
+    println("Double Value : $d ")
+    var l : Long = 338927847
+    println("Long Value : $l ")
+    var h : Short = -2
+    println("Short Value : $h ")
+    var a : Byte = 127
+    println("Byte Value : $a ")
+}
